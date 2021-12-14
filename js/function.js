eaba8a933198f1451a64c8fa9aab0ceb7591f7a9
gabrizz8998 <91249733+gabrizz8998@users.noreply.github.com>
2021-12-14 17:21:19 +0100

    b

--- js/function.js
+++ js/function.js
@@ -1,90 +1,104 @@
-let texto_resultado=document.getElementById("texto_resultado");
+let texto_resultado = document.getElementById("texto_resultado");
 let casillas = document.getElementsByTagName("td");
-for (let i = 0; i < casillas.length-1; i++) {
+for (let i = 0; i < casillas.length - 1; i++) {
   casillas[i].addEventListener("click", mostraralerta, false);
 }
+
 let resultado = 0;
 let operador = 1;
 let operacion = "+";
 let operando1 = "";
 let operando2 = "";
 let dato;
+let operacion_anterior = "";
+
 function mostraralerta() {
-    
   dato = this.innerHTML;
+  console.log(dato);
   switch (dato) {
     case "+":
+      operacion_concatenada();
       operacion = "+";
       operador = 2;
       break;
     case "-":
+      operacion_concatenada();
       operacion = "-";
       operador = 2;
       break;
     case "*":
+      operacion_concatenada();
       operacion = "*";
       operador = 2;
       break;
     case "/":
+      operacion_concatenada();
       operacion = "/";
       operador = 2;
       break;
     case "CE":
-        resultado = 0;
-        operador = 1;
-        operacion = "+";
+      if (operador == 1) {
         operando1 = "";
+      } else {
         operando2 = "";
-        dato;
-        texto_resultado.innerHTML="Resultado";
-        break;
-    case "C":
-        if(operador == 1){
-            operando1="";
-        }
-        else{
-            operando2="";
-        }
-        break;
-    case "=":
-      operando1 = parseFloat(operando1);
-      operando2 = parseFloat(operando2);
-      switch (operacion) {
-        case "+":
-          resultado = operando1 + operando2;
-          break;
-        case "-":
-          resultado = operando1 - operando2;
-          break;
-        case "*":
-          resultado = operando1 * operando2;
-          break;
-        case "/":
-          resultado = operando1 / operando2;
-          break;
       }
-      resultado=resultado.toFixed(2);
-      texto_resultado.innerHTML="Resultado = "+resultado;
-      operando1 ="";
-      operando2 ="";
+      break;
+    case "C":
+      resultado = 0;
+      operador = 1;
+      operacion = "+";
+      operando1 = "";
+      operando2 = "";
+      dato;
+      texto_resultado.innerHTML = "0";
+      break;
+    case "=":
+      resultado = calcular();
+      texto_resultado.innerHTML = resultado;
+      operando1 = "";
+      operando2 = "";
       operador = 1;
       break;
     default:
-        if(resultado!=0){
-            operando1=resultado.toString();
-          }else{
-            if (operador == 1) {
-                operando1 += dato;
-          }
-          }
-          if (operador == 2) {
-            operando2 += dato;
-          }
+      if (resultado != 0) {
+        operando1 = resultado.toString();
+      } else {
+        if (operador == 1) {
+          operando1 += dato;
+        }
+      }
+      if (operador == 2) {
+        operando2 += dato;
+      }
       break;
   }
 }
 
+function calcular() {
+  let resultado;
+  operando1 = parseFloat(operando1);
+  operando2 = parseFloat(operando2);
+  switch (operacion) {
+    case "+":
+      resultado = operando1 + operando2;
+      break;
+    case "-":
+      resultado = operando1 - operando2;
+      break;
+    case "*":
+      resultado = operando1 * operando2;
+      break;
+    case "/":
+      resultado = operando1 / operando2;
+      break;
+  }
+  return resultado.toFixed(2);
+}
 
-function calcular(){
-    
+function operacion_concatenada() {
+  if (operando2 != "") {
+    operando1 = calcular().toString();
+    texto_resultado.innerHTML = operando1;
+    operando2 = "";
+  }
 }
